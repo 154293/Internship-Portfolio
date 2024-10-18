71e405fc53d3b36f73384db9cd5e0314b4cba0ae
SVEN KEIJZER <154293@student.horizoncollege.nl>
2024-10-18 15:23:20 +0200

    Made Calculator more efficient

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Internship-Portfolio
+This portfolio is to show a few different skills I've been working on.
+I have mostly been investing time into PHP and its capabilities, but I also have some basic frontend knowledge and I've been able
+to try out some other programming languages such as JS, Python and C# a tiny bit.
+On those latter few languages, you'll see that my level is pretty basic so far, but I'll likely by spending more time learning
+about those down the line.
+
+The folders in the portfolio are sorted by programming language/technique used.
+In the 'Study Exercises' folder, you'll find tiny programs I've had to make for my study.
+In the 'Personal Project' folder, you'll find a project I've been working on in my spare time.
+
+Should you find a README file in the folder of one of the exercises, it'll likely contain some further context and explanation
+as to what the purpose and limitations of the exercise specifically were.
+In the folder of my personal project, you'll also find more info on what it's for and what some of my stretchgoals are.
+
+If there's any questions or critiques about specific pieces of code that the comments haven't been able to clear up,
+feel free to ask me by reaching out through email, or bringing it up in conversation.
--- Study Exercises/C#/C#-3-7-Calculator/Program.cs
+++ Study Exercises/C#/C#-3-7-Calculator/Program.cs
@@ -1,80 +1,72 @@
-﻿float getal_1 = 0f;
-float getal_2 = 0f;
+﻿float getal1 = 0f;
+float getal2 = 0f;
+float resultaat = 0f;
 string input1, input2;
-float resultaat = 0;
-bool isnumeric;
-Console.WriteLine("Operatie?");
+Console.WriteLine("Operatie?");          //asks user for operator
 string operatie = Console.ReadLine();
-if (operatie == "+" || operatie == "-" || operatie == "*" || operatie == "/" || operatie == "%")
+if (operatie == "+" || operatie == "-" || operatie == "*" || operatie == "/" || operatie == "%")    //array with valid operators
 {
+    static float CheckNumber(string input)
+    {
+        try
+        {
+            return float.Parse(input);                   //checknumber method tries making input into float
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(input + " is geen geldig getal!");   //otherwise, it quits the application
+            Environment.Exit(0);
+        }
+        return 0f;
+    }
+
     Console.WriteLine("Eerste getal?");
-    input1 = Console.ReadLine();
-    try
-    {
-        getal_1 = float.Parse(input1);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine("Dat is geen geldig nummer");
-        Environment.Exit(0);
-    }
+    input1 = Console.ReadLine();                //both inputs get ran through the method
+    getal1 = CheckNumber(input1);
+
     Console.WriteLine("Tweede Getal?");
     input2 = Console.ReadLine();
-    try
+    getal2 = CheckNumber(input2);
+
+    if (operatie == "+" || operatie == "-" || operatie == "*")
     {
-        getal_2 = float.Parse(input2);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine("Dat is geen geldig nummer");
-        Environment.Exit(0);
-    }
-    if (operatie == "+")
-    {
-        resultaat = getal_1 + getal_2;
-        Console.WriteLine("Resultaat:");
-        Console.WriteLine(resultaat);
-    }
-    else if (operatie == "-")
-    {
-        resultaat = getal_1 - getal_2;
-        Console.WriteLine("Resultaat:");
-        Console.WriteLine(resultaat);
-    }
-    else if (operatie == "*")
-    {
-        resultaat = getal_1 * getal_2;
-        Console.WriteLine("Resultaat:");
-        Console.WriteLine(resultaat);
-    }
-    else if (operatie == "/")
-    {
-        if (getal_2 != 0)
+        switch (operatie)
         {
-            resultaat = getal_1 / getal_2;
-            Console.WriteLine("Resultaat:");
-            Console.WriteLine(resultaat);
+            case "+":
+                resultaat = getal1 + getal2;
+                break;
+            case "-":
+                resultaat = getal1 - getal2;          //switch case for the basic operators
+                break;
+            case "*":
+                resultaat = getal1 * getal2;
+                break;
+        }
+        string output = resultaat.ToString("0.############");    //my attempt at making the float output work (it didn't)
+        Console.WriteLine("Resultaat: " + output);
+    }
+    else if (operatie == "/" || operatie == "%")
+    {
+        if (getal2 != 0)                     //for the other operators, there's an extra check to see if the second number is 0
+        {
+            switch (operatie)
+            {
+                case "/":
+                    resultaat = getal1 / getal2;        
+                    break;
+                case "%":
+                    resultaat = getal1 % getal2;
+                    break;
+            }
+            Console.WriteLine("Resultaat: " + resultaat);
         }
         else
-        {
-            Console.WriteLine("Kan niet delen door 0");
-        }
-    }
-    else if (operatie == "%")
-    {
-        if (getal_2 != 0)
-        {
-            resultaat = getal_1 % getal_2;
-            Console.WriteLine("Resultaat:");
-            Console.WriteLine(resultaat);
-        }
-        else
-        {
+        {                                                       //if it is a 0, it can't be used in division
             Console.WriteLine("Kan niet delen door 0");
         }
     }
 }
 else
-{
+{                                                                  //if it's none of the mentioned operators, it can't be used
     Console.WriteLine(operatie + " is geen geldige operatie");
 }
