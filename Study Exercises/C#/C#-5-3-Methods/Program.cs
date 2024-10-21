3cd6763b6bd1bdaadbd0bbee85191279e16b7742
SVEN KEIJZER <154293@student.horizoncollege.nl>
2024-10-21 11:13:28 +0200

    Added 'C# Methods' exercise

---
+++ Study Exercises/C#/C#-5-3-Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] results;
+results = Calculations.Resultaten(5);
+Console.WriteLine($"Volume = {results[0]} en oppervlakte = {results[1]}");
+
+results = Calculations.Resultaten(10);
+Console.WriteLine($"Volume = {results[0]} en oppervlakte = {results[1]}");
+
+results = Calculations.Resultaten(20);
+Console.WriteLine($"Volume = {results[0]} en oppervlakte = {results[1]}");
+class Calculations
+{
+    public static int VolumeKubus(int lengte)
+    {
+        return lengte * lengte * lengte;
+    }
+
+    public static int OppKubus(int lengte)
+    {
+        return 6 * lengte * lengte;
+    }
+
+    public static int[] Resultaten(int lengte)
+    {
+        int volRes = Calculations.VolumeKubus(lengte);
+        int oppRes = Calculations.OppKubus(lengte);
+
+        return new[] { volRes, oppRes };
+    }
+}
